bd579198b6341aa4e93f8c8515180677ff06d28d
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2021-01-02 17:38:40 +0100

    work on btn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 
+
+const nav = document.querySelector('.nav-list');
+const burger = document.querySelector('.btn-burger-menu');
+const navLinks = document.querySelectorAll('.__item a');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-open');
+  burger.classList.toggle('toggle');
+})
+
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.toggle('nav-open');
+    burger.classList.toggle('toggle');
+  })
+})
